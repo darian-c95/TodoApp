e3a20b49b17de79cbc7a5f6f0e52c443e5550cc4
Dari <dari_c95@outlook.com>
2022-04-27 16:12:59 -0300

    Actualiza index.js

--- src/index.js
+++ src/index.js
@@ -14,29 +14,3 @@ root.render(
     </ChakraProvider>
   </React.StrictMode>
 );
-
-
-
- 
-
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client'; 
-// import App from './App';
-// import reportWebVitals from './reportWebVitals'; 
-// import { ChakraProvider } from '@chakra-ui/react';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode> 
-//     <ChakraProvider>
-//       <App /> 
-//     </ChakraProvider>
-//   </React.StrictMode>
-// );
-
-
-// reportWebVitals();
- 
